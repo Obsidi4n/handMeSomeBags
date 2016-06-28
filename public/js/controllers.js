73a779db51182a9a9aa628f217dcadf6f6699ac2
Rohith <rohith.pune@gmail.com>
2016-06-28 23:16:07 +0530

    Update

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,16 +19,18 @@ var authModule = angular.module('Authentication', [])
 .controller('AuthController', ['$scope', '$http', '$location', '$window', function ($scope, $http, $location, $window){
     
     $scope.login = function() {
-        $scope.dataLoading = true;
-        $http.post('/login', {user: $scope.username, pass: $scope.password})
+        // $scope.dataLoading = true;
+        $http.post('/public/login', {username: $scope.username, password: $scope.password})
             .success(function(data) {
-                console.log(data);
-                $window.location.href = data;
+                $window.sessionStorage.token = data.token;
+                $window.sessionStorage.expires = data.expires;
+                $window.sessionStorage.user = data.user;
+                $window.location.href = data.location+'?token='+data.token;
             })
             .error(function(data) {
                 $scope.form.$setPristine();
                 $scope.dataLoading = false;
-                $scope.error = data;
+                $scope.error = data['error'];
             });
     };
     $scope.register = function(){
@@ -40,22 +42,24 @@ var authModule = angular.module('Authentication', [])
 
 .controller('RegisterController', ['$scope', '$http', '$location', '$window', function ($scope, $http, $location, $window){
     $scope.register = function(){
-        $scope.dataLoading = true;
-        $http.post('/register', 
-            {   user: $scope.username, 
-                pass: $scope.password,
+        // $scope.dataLoading = true;
+        $http.post('/public/register', 
+            {   username: $scope.username, 
+                password: $scope.password,
                 email: $scope.email,
                 contact: $scope.contact
             })
             .success(function(data) {
-                $scope.error = data;
-                alert('Successfully added');
-                $window.location.href = '/login';
+                $window.sessionStorage.token = data.token;
+                $window.sessionStorage.expires = data.expires;
+                $window.sessionStorage.user = data.user;
+                $window.location.href = data.location+'?token='+data.token;
             })
             .error(function(data) {
+                console.log('Error in registering: '+ data);
                 $scope.form.$setPristine();
                 $scope.dataLoading = false;
-                $scope.error = data;
+                $scope.error = data['error'];
             });
     };
     console.log('RegisterController ready');
--- server.js
+++ server.js
@@ -1,90 +1,47 @@
-var db = require('./public/js/db');
 var express = require('express');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
+var config = require('./config/config.js');
+var mongoose = require('mongoose');
+
+
 // var session = require('cookie-session');
 app = express();
 
-app.use(express.static(__dirname +'/public'));
-app.use(bodyParser.json());
+//app.use(logger('combined'));    //for logging requests
 
-app.set('authFlag',1);
-app.disable('authFlag');
-var auth = function(req, res, next){ 
-	if (!app.get('authFlag') )//&& req.session.authToken === 'logged' ) 
-		res.sendFile('unauthorized.html', { root: __dirname+'/public/pages' }); 
-	else 
-		next(); 
-	};
+var publicRoutes = require('./routes/publicRoutes');
+var publicApis = require('./routes/publicApis');
+var authorizedRoutes = require('./routes/authorizedRoutes');
+var restrictedApis = require('./routes/restrictedApis') 
 
-app.get('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname+'/public' });
-});
+var validateRequest = require('./middleware/validateRequest');
 
-app.post('/login',function(req,res){
-	db.User.find({username: req.body.user, password: req.body.pass}, function(err,user){
-	if ( user.length || (req.body.user === 'admin' &&  req.body.pass === 'secret') )
-	{
-		console.log('Logging in');
-		if(user.length)
-			console.log(user);
-		app.enable('authFlag');
-		// req.session.authToken = 'logged';
-		res.send('/mainPage');
-	}		
-	else if(err)
-		res.status(500).send('Db error. Please check with admin');
-	else
-		res.status(401).send('Incorrect credentials');
-  });
-});
 
-app.get('/mainPage', auth, function(req, res) {
-    res.sendFile('main.html', { root: __dirname+'/public/pages' });
-});
 
-app.get('/logout',function(req,res){
-	console.log('Logging out');
-	app.disable('authFlag');
-	//delete req.session.authToken;
-	console.log('Auth Flag: '+app.get('authFlag'));
-	res.send('/');
-});
+app.use(express.static(__dirname +'/public')); // serving public folder
 
-app.get('/register', function(req, res) {
-    res.sendFile('register.html', { root: __dirname+'/public/pages' });
-});
+app.use('/', publicRoutes.router); //mount all public routes
 
-app.post('/register',function(req,res){
-	var b = req.body;
-	console.log('Trying to register '+ b.user);
-	if(b.user && b.pass && b.email && b.contact)
-	{
-		new db.User({
-			username: b.user,
-		    password:b.pass,
-		    email:b.email,
-		    contact:b.contact
-		}).save(function(e, u){
-			if(e)
-				res.status(500).send('Error adding user. Please check with administrator.');
-			else
-			{
-				// console.log(db.User.find());
-				console.log(u);
-				res.send('User successfully added!');
-				console.log('Users: ' + db.User.find().length); 
-			}				
-		});
-	}
-	else
-		res.status(403).send('User values missing. Please check');
-});
+app.use('/public', publicApis.router); //mount public API calls here
+
+app.use('/logged', validateRequest,  authorizedRoutes.router); //mount authorized entry pages here
+
+app.use('/restricted', restrictedApis.router); //mount authorized API calls here
+
+/*
+currently has /register and /login end points, returns json with a
+access token which has to be sent along with
+*/
 
 app.get('*',function(req,res){
-	console.log("Mr.Anderson, you have to stay within the system");
-	res.redirect('/');
-})
+    console.log("Mr.Anderson, you have to stay within the system");
+    res.redirect('/');
+});
 
-var server = app.listen(7896);
-console.log("I'm ready");
-console.log('Users: ' + db.User.find().length);
+
+
+mongoose.connect(config.mongo_url, function(){
+    var server = app.listen(7896);
+    console.log("I'm ready, listening on 7896");
+
+})
